57a9862d164348964cd8cf0ae8a811f4ec90eeb0
Your Name <you@example.com>
2025-07-28 12:27:10 +0300

    Remove unused test files, logo, and web vitals; add API and component files for articles and categories

---
+++ src/component/AddArticleForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AddArticleForm = () => {
+  return (
+    <div>AddArticleForm</div>
+  )
+}
---
+++ src/component/EditArticleForm.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const EditArticleForm = () => {
+  return (
+    <div>EditArticleForm</div>
+  )
+}
