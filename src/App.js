e18b5e9e3444d55786e3599743983823eae0b82a
Your Name <you@example.com>
2025-07-31 16:00:32 +0300

    Refactor App component to integrate routing and add Header; create Hero and ListOfNews components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Header } from './component/Header';
+import { Home } from './pages/Home';
+import { AddArticle } from './pages/AddArticle';
+import { EditArticle } from './pages/EditArticle';
+import { Article } from './pages/Article';
 
+import { Routes, Route } from 'react-router-dom';
+// Importing API functions
+import { getArticles } from './api/articles'; // Adjust the import path as needed
+import { getCategories } from './api/categories'; // Adjust the import path as needed
+
+
+import { useState, useEffect } from 'react';
 function App() {
+    const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  const fetchAndSet = async (apFn, setter, label) => {
+    try {
+      setLoading(true);
+      const response = await apFn();
+      console.log(`✅ ${label} fetched`);
+      setter(response.data);
+    } catch (error) {
+      console.error(`Error fetching ${label}:`, error);
+      console.error(`❌ ${label} Error:`, error.message);
+      console.log("🔍 URL:", error.config?.baseURL + error.config?.url);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAndSet(getArticles, setArticles, 'articles');
+    fetchAndSet(getCategories, setCategories, 'categories');
+  }, []);
   return (
     <div className="App">
+
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home articles={articles} loading={loading} categories={categories} />} />
+        <Route path="/add-article" element={<AddArticle />} />
+        <Route path="/edit-article/:id" element={<EditArticle />} />
+        <Route path="/article/:id" element={<Article />} />
+      </Routes>
       
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Hero } from '../component/Hero'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <>
+     <Hero />
+
+     
+    </>
+
   )
 }
